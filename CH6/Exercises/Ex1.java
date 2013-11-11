621c4f94a944bffcd66c72244f82f1b9a6b4c340
Jordan Lilje <jlilje@java.nas.local>
2013-11-11 10:48:19 -0500

    add Ex1.java

---
+++ CH6/Exercises/Ex1.java
@@ -0,0 +1,25 @@
+class Exercise1 {
+
+  public static void main(String[] args){
+   int a;
+   int b;
+   isDivisible(8,4);
+ }
+  public static void isDivisible(int a, int b){
+     if (a%b == 0){
+      System.out.println("The equation is: true");
+    }else{
+      System.out.println("The equation is: false");
+   }
+  }
+ }
+
+/*public class Exercise01 {
+    public static boolean isDivisible(int n, int m) {
+      if (n%m == 0) {
+        return true;
+  }
+        return false;
+ }
+}
+*/
