6483de1238e010a234fa0862fc6a76c9c7f1f463
Jordan Lilje <jlilje@java.nas.local>
2013-11-13 10:11:21 -0500

    Added Exercise 6.2

--- CH6/Exercises/Ex1.java
+++ CH6/Exercises/Ex1.java
@@ -5,21 +5,11 @@ class Exercise1 {
    int b;
    isDivisible(8,4);
  }
-  public static void isDivisible(int a, int b){
+  public static boolean isDivisible(int a, int b){
      if (a%b == 0){
-      System.out.println("The equation is: true");
+      return true;
     }else{
-      System.out.println("The equation is: false");
+      return false;
    }
   }
  }
-
-/*public class Exercise01 {
-    public static boolean isDivisible(int n, int m) {
-      if (n%m == 0) {
-        return true;
-  }
-        return false;
- }
-}
-*/
