4108c76072152d9cfab042687daa1723ea7f3aa9
Jordan Lilje <jlilje@java.nas.local>
2013-11-20 09:58:09 -0500

    added Exercise 1

---
+++ CH6/Exercises/Lesson6/Ex1.java
@@ -0,0 +1,15 @@
+class Exercise1 {
+
+  public static void main(String[] args){
+   int a;
+   int b;
+   isDivisible(8,4);
+ }
+  public static boolean isDivisible(int a, int b){
+     if (a%b == 0){
+      return true;
+    }else{
+      return false;
+   }
+  }
+ }
