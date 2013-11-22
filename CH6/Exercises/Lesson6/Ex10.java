889365218dc6eeeffe9e4abb952d20a36c215c6d
Jordan Lilje <jlilje@java.nas.local>
2013-11-22 13:12:52 -0500

    added Ex10

---
+++ CH6/Exercises/Lesson6/Ex10.java
@@ -0,0 +1,11 @@
+class Ex10 {
+  public static void main(String[] args){
+    System.out.println(isDivisible(0,1));
+}
+  public static boolean isDivisible(int n, int m) {
+    if (n%m == 0) {
+      return true;
+     }else{
+      return false;
+}}}
+
